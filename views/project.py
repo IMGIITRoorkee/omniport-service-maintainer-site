d12223a1e4a12cb1b22542a27cfe75e3f63e8939
Harshit Khetan <khetanmehul@gmail.com>
2019-01-12 23:32:46 +0530

    Make project view ReadOnly or must be maintainer to post data

--- views/project.py
+++ views/project.py
@@ -1,13 +1,22 @@
 from rest_framework import viewsets
 from maintainer_site.serializers.project import ProjectSerializer
 from maintainer_site.models.project import Project
+from rest_framework import permissions
+from rest_framework.permissions import BasePermission, IsAuthenticated
+from kernel.permissions.has_role import get_has_role
 
+
+
+class ReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.method in permissions.SAFE_METHODS
 
 class ProjectViewSet(viewsets.ModelViewSet):
     """
-    A viewset for viewing and editing all the Projects
+    API endpoint allows ProjectModel to be viewed or edited
     """
-
+   
+    permission_classes = (get_has_role('Maintainer')|ReadOnly,)
     serializer_class = ProjectSerializer
     queryset = Project.objects.all().order_by('-datetime_created')
     
