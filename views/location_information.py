03c761241fed9a9fe4b6f5744fa2495421568d28
Arpit Gupta <guptarpit1997@gmail.com>
2020-01-12 04:45:06 +0530

    Concisely send country name in the response

--- views/location_information.py
+++ views/location_information.py
@@ -1,6 +1,7 @@
 from django.http import Http404
 
 from rest_framework import generics
+from rest_framework.response import Response
 
 from maintainer_site.models.maintainer_group import MaintainerGroup
 from formula_one.serializers.generics.location_information import (
@@ -14,6 +15,24 @@ class LocationInformationView(generics.RetrieveAPIView):
     """
 
     serializer_class = LocationInformationSerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Override retrieve to concisely send the country name
+        :return: the location information with the country detailq
+        """
+        location_information = LocationInformationSerializer(
+            self.get_object()
+        ).data
+        response = {}
+        for location_attribute in location_information.keys():
+            if location_attribute == 'country_detail':
+                response['country'] = \
+                    location_information[location_attribute]['name']
+            else:
+                response[location_attribute] = \
+                    location_information[location_attribute]
+        return Response(response)
 
     def get_object(self, *args, **kwargs):
         """
