2b91c2b218d5ec6d390750c84e346bddea913f1e
bbahd30 <b_bonal@cs.iitr.ac.in>
2023-02-15 18:51:26 +0530

    Filter Blogs based on Tags

--- views/blog.py
+++ views/blog.py
@@ -27,24 +27,85 @@ class BlogView(APIView):
             f"https://medium.com/feed/{pub_id}"
         )
         response = requests.get(url)
-        required_data_posts = [
-            "author",
-            "title",
-            "pubDate",
-            "link",
-            "thumbnail",
-        ]
-        sanitized_response = []
         blog_list = json.loads(response.content).get("items")
         max_blog_count = 6
         if response.status_code == 200:
             required_blog_list = blog_list[:max_blog_count]
         else:
             return Response(status=status.HTTP_204_NO_CONTENT)
-        for blog in required_blog_list:
-            sanitized_content = {}
-            for item in required_data_posts:
-                sanitized_content[item] = blog.get(item)
-            sanitized_response.append(sanitized_content)
+        # for blog in required_blog_list:
+        #     sanitized_content = {}
+        #     for item in required_data_posts:
+        #         sanitized_content[item] = blog.get(item)
+        #     sanitized_response.append(sanitized_content)
+        # print(sanitized_response)
+        # return Response(sanitized_response)
+
+        category_dict = {"design": [], "development": [], "culture": []}
+        
+        sanitized_response = BlogView.tag_segregator(required_blog_list, category_dict)
+        
+
+        two_for_each = BlogView.tag_count_checker(sanitized_response)
+
+        while two_for_each == False:
+            print("****")
+            required_blog_list = blog_list[max_blog_count: (max_blog_count+6)]
+            sanitized_response = BlogView.tag_segregator(required_blog_list, category_dict)
+            two_for_each = BlogView.tag_count_checker(sanitized_response)
+            print(two_for_each)
 
         return Response(sanitized_response)
+
+    def tag_count_checker(sanitized_response):
+        """
+        Function to find whether each category has atleast 2 blogs to display
+        """
+
+        for i in range(len(sanitized_response)):
+            if len(sanitized_response[i]["blogs_list"]) < 2:
+                return False
+        return True
+    
+    def tag_segregator(required_blog_list, category_dict):
+        """
+        Function to segregate the blogs into design, development and culture tags
+        """
+
+        categories = ["design", "development"]
+        for blog in required_blog_list:
+            found_tag = False
+            for tag in categories:
+                blog_data = BlogView.get_sanitized_blog(blog)
+                if tag in blog["categories"]:
+                    category_dict[tag].append(blog_data)
+                    found_tag = True
+            if not found_tag: 
+                category_dict["culture"].append(blog_data)
+                    
+        sanitized_response = []
+        for category, blogs in category_dict.items():
+            temp = {}
+            temp['category'] = category
+            temp['blogs_list'] = blogs
+            sanitized_response.append(temp)
+
+        return sanitized_response
+
+    def get_sanitized_blog(blog):
+        """
+        Returns a dictionary of required data for a blog
+        """
+        
+        required_data_posts = [
+            "author",
+            "title",
+            "pubDate",
+            "link",
+            "thumbnail",
+            "categories",
+        ]
+        sanitized_content = {}
+        for item in required_data_posts:
+            sanitized_content[item] = blog.get(item)
+        return sanitized_content
